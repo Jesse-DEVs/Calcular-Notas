35b897ccb07676ec8e5a92425b55355bd736b04c
Jessé Miguel Ramos <jesse.miguel.2003@gmail.com>
2025-03-25 20:55:32 -0300

    Refatorar cálculo de notas: criar função para obter notas e simplificar lógica de aprovação

--- Calculo_de_nota.py
+++ Calculo_de_nota.py
@@ -1,51 +1,31 @@
-while True:
-    nome = input(' Digite Seu Nome Completo: ')
+def obter_nota(materia):
+    while True:
+        try:
+            nota = float(input(f'Digite a nota de {materia} (0 a 10): '))
+            if 0 <= nota <= 10:
+             return nota
+            else:
+                print('a nota deve ser entre 0 e 10, tente novamente')
+        except ValueError: 
+            print('entrada invalida. digite um número entre 0 e 10')      
+def calcular (): 
+    print('Bem vindo ao sistema de calculo de notas')
+    nome = input('Digite seu nome completo: ')
     
-    nota_Mat = float(input(' Digite Sua Nota de Matematica (exemplo: 10.0) '))
+    materia = ['Matemática', 'Português', 'Inglês', 'História']
+    notas = [] 
+    for materia in materia:
+        notas.append(obter_nota(materia))
+    media = sum(notas) / len(notas)
     
-    if 0 <= nota_Mat <10.1:
-        break
+    print(f'\nAluno: {nome}')
+    print(f'media: {media:.1f}')
+    if media >= 5:
+        print('Aprovado')
     else:
-        print('Digite um numero valido! ')
-while True:
-    nota_Port = float(input(' Digite Sua Nota de Portugues (exemplo: 10.0): '))
-    
-    if 0 <= nota_Port <10.1:
-        break
-    else:
-        print('Digite um numero valido! ')
-while True:
-    nota_Ingles = float(input(' Digite Sua Nota de Ingles (exemplo: 10.0): '))
-
-    if 0 <= nota_Ingles <10.1:
-        break
-    else:
-        print('Digite um numero valido! ')
+        print('Reprovado')
         
-while True:
-    nota_Hist = float(input(' Digite Sua Nota de Historia (exemplo: 10.0): '))
-
-    if 0 <= nota_Hist <10.1:
-        break
-    else:
-        print('Digite um numero valido! (exemplo: 10.0): ')
-
-
-print(f' Ola {nome}, Tudo bem ?')
-print(f'Nota de Matematica: {nota_Mat}')
-print(f'Nota de Portugues: {nota_Port}')
-print(f'Nota de Inglês: {nota_Ingles}')
-print(f'Nota de Historia: {nota_Hist}')
-
-Soma = nota_Mat + nota_Port + nota_Ingles + nota_Hist
-
-resultado = Soma/4 
-print(f'{nome} Sua media de nota foi: {resultado:.1f}')
-
-if (resultado <= 5 ):
-    print(f'{nome} Infelizmente voce foi reporvado')
-
-elif (resultado > 5 ):
-    print(f'{nome} Parabéns Você foi Aprovado!!')
+if __name__ == '__main__':
+    calcular()
     
-    
+   
